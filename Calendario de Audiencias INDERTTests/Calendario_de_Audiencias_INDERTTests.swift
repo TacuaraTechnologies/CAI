cb2014502a22ecb54d2c82b1f0d7d461fb83e383
Javier Rivarola <javierrivarola@tacuaratech.com>
2015-07-27 19:29:52 -0400

    Initial Commit

---
+++ Calendario de Audiencias INDERTTests/Calendario_de_Audiencias_INDERTTests.swift
@@ -0,0 +1,36 @@
+//
+//  Calendario_de_Audiencias_INDERTTests.swift
+//  Calendario de Audiencias INDERTTests
+//
+//  Created by Javier Rivarola on 27/Jul/15.
+//  Copyright (c) 2015 Tacuara Technologies. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Calendario_de_Audiencias_INDERTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
